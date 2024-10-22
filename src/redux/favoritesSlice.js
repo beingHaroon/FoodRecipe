03db2f486a365b2c5965529fbde7b45db5dbe137
beingHaroon <haroon_md@skillup.online>
2024-10-22 11:37:56 -0400

    initialcommit

--- src/redux/favoritesSlice.js
+++ src/redux/favoritesSlice.js
@@ -8,7 +8,17 @@ const favoritesSlice = createSlice({
   name: "favorites",
   initialState,
   reducers: {
-   
+    toggleFavorite(state, action) {
+      const recipeId = action.payload.idFood;
+      const existingRecipe = state.favoriterecipes.find((recipe) => recipe.idFood === recipeId);
+
+      if (existingRecipe) {
+        state.favoriterecipes = state.favoriterecipes.filter((recipe) => recipe.idFood !== recipeId);
+      } else {
+        state.favoriterecipes.push(action.payload);
+      }
+      console.log('favoriterecipes:', state.favoriterecipes); // Add this line
+    },
   },
 });
 
